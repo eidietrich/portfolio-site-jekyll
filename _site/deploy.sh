1c72cb9ca2edb485ab2344e4b60acf8931847a56
Eric Dietrich <eric.dietrich@gmail.com>
2018-07-02 20:06:35 -0600

    Refresh site

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -2,16 +2,27 @@
 # Script for automating deployment from development repository
 # To eidietrich.github.io
 
-BUILDDIR=./_site/.
-DESTDIR=./../eidietrich.github.io
-RETURNPATH=./../portfolio-site-jekyll
+# bash deploy.sh
 
-jekyll build
+# BUILDDIR=./_site/.
+# DESTDIR=./../eidietrich.github.io
+# RETURNPATH=./../portfolio-site-jekyll
+
+# jekyll build
+
+# cp -R $BUILDDIR $DESTDIR
+# cd $DESTDIR
+# git add *
+# git commit -m "Scripted update"
+# git push
+# cd $RETURNPATH
+# echo "deploy complete"
+
+# New deployment process
+# Ref https://gist.github.com/tduarte/eac064b4778711b116bb827f8c9bef7b
+
 echo "ericidietrich.com" >> ./_site/CNAME
-cp -R $BUILDDIR $DESTDIR
-cd $DESTDIR
-git add *
-git commit -m "Scripted update"
-git push
-cd $RETURNPATH
-echo "deploy complete"
+
+git subtree split --prefix _site -b gh-pages
+git push -f prod gh-pages:master
+git branch -D gh-pages
